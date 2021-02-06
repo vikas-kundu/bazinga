904d74fba6f10992f3884f538efa0b430312e371
vikas-kundu <47297799+vikas-kundu@users.noreply.github.com>
2021-02-06 13:22:18 +0530

    install file

---
+++ install.sh
@@ -0,0 +1,7 @@
+#install node
+curl -sL https://deb.nodesource.com/setup_15.x | sudo -E bash - 
+sudo apt-get install -y nodejs
+#npm install ngrok
+sudo npm i ngrok
+#npm install yargs
+sudo npm i yargs
