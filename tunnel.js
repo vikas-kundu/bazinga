371b29fbb2a486040fbfde1e666820de6dc67ce9
vikas-kundu <47297799+vikas-kundu@users.noreply.github.com>
2021-02-25 13:55:11 +0530

    typo fix

--- tunnel.js
+++ tunnel.js
@@ -4,7 +4,6 @@ exports.startTunnel= function (options,tunnel_url){
 	(async function(){
 		console.log("Info:Please be patient, requesting for new tunnel can take a few minutes...");
 		const url = await ngrok.connect({addr:options.port});
-		console.log("here");
 		if(url){
 			console.log("Info:Ngrok Tunnel Obtained!");
 			console.log("Warning:Please note all the links will be defunct once Ngrok is stopped."); 
